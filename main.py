ff35f5bb028cfdd1e2920fac974e67b066b3321a
Shruti627 <shrutisangvikar627@gmail.com>
2025-06-16 12:55:50 +0530

    new api commit

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class LoginBody(BaseModel):
+    email: str
+    password: str
 
 app = FastAPI()
 
-
 @app.get("/hello")
 def read_root():
-    return {"msg": " Hello World"}
+    return {"msg": "Hello World"}
 
-
+@app.post("/login")
+def login(loginBody: LoginBody):
+    print(loginBody.email)
+    return {
+        "msg": "Login successfully"
+    }
